46a4c155f1e44dd078b389615af5c87e6a52caad
Florent <florentg233@gmail.com>
2025-04-30 01:21:33 +0200

    changed the image just to test git pull for arian

--- sms-frontend/app/page.tsx
+++ sms-frontend/app/page.tsx
@@ -310,7 +310,7 @@ export default function Home() {
                 <div className="relative">
                   <div className="absolute -top-4 -right-4 h-72 w-72 bg-primary/10 rounded-full blur-3xl"></div>
                   <img
-                    src="https://www.kekamed.com/eduford_img/homekeka.jpg"
+                    src="https://images.unsplash.com/photo-1591123120675-6f7f1aae0e5b?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="About SchoolMS"
                     className="relative rounded-lg shadow-xl object-cover border"
                     width={500}
